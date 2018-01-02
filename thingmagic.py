754825abd0fe4e278fad246bc5b89ccbaad0369d
David Hendry <chain_default@hotmail.com>
2018-01-02 02:12:11 -0700

    further trimming

--- thingmagic.py
+++ thingmagic.py
@@ -14,11 +14,9 @@ GPIO.setup(13,GPIO.OUT)
 GPIO.setup(19,GPIO.OUT)
 GPIO.setup(26,GPIO.OUT)
 
-#maps rfid count to appropiate GPIO pin
-gpio_output_list = [5, 6, 13, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26]
+#maps rfid tag count to appropiate GPIO pin
+gpio_output_list = [5, 6, 13, 19, 26]
 
-
-## Everythin in main to be looped.
 def main():
 
     previous_tag_count=(len(reader.read(timeout=50)))    
@@ -30,34 +28,17 @@ def main():
     print (current_tag_count)
 
     if current_tag_count==previous_tag_count:
-        print("No change detected")
+        print("No Change Detected")
+
+    #protect against count outside range of list
+    if current_tag_count > 4:
+        current_tag_count = 4
     else:
         print("Change Detected")       
-        GPIO.output(gpio_output_list[previous_tag_count],True)
+        GPIO.output(gpio_output_list[current_tag_count],True)
         sleep(.1)
-        GPIO.output(gpio_output_list[previous_tag_count],False)
-
-def verifyreader():
-    #To determine if rfid reader is responding correctly.
-    readermodel = (reader.get_model())
-    if (readermodel=="M6e Nano"):
-        print ("Found M6e Nano")
-        return True
-    else:
-        #reboots rasbperry pi which also reboots the reader.
-        print ("-Unable to detect 'M6e Nano' rfid reader.")
-        sleep(2)
-        print ("-Rebooting Rasberry Pi.")
-        sleep(5)
-        subprocess.Popen(["sudo", "reboot"])
+        GPIO.output(gpio_output_list[current_tag_count],False)
 
 
-
-
-
-while verifyreader()==True:
+while True==True:
     main()
-
-        
-
-
